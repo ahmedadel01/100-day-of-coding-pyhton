2f9f0bae4f7766c3896a74c22e9b26a153926ff3
ahmed adel <71907551+ahmedadel01@users.noreply.github.com>
2022-04-28 15:03:55 +0200

    Create No_Idea.py

---
+++ Day3/No_Idea.py
@@ -0,0 +1,9 @@
+""" https://www.hackerrank.com/challenges/no-idea/problem?isFullScreen=true """
+
+n, m = input().split()
+
+sc_ar = input().split()
+
+A = set(input().split())
+B = set(input().split())
+print(sum([(i in A) - (i in B) for i in sc_ar]))
