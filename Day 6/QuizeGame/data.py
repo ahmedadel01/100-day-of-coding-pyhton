30433f77fd35294700c93e3de5eb4f25b9a74878
ahmed adel <71907551+ahmedadel01@users.noreply.github.com>
2022-05-02 18:31:30 +0200

    Create data.py

---
+++ Day 6/QuizeGame/data.py
@@ -0,0 +1,13 @@
+questionData = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Aproximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface aarea of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In west virginia, USA, if you accidentally hit an animal with your car,\
+            you are free to take it hime to eat.", "answer": "True"},
+    {"text": "In london, UK, if you happen to die in the house of parliament, \
+            you are entitled to a state funeral.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'", "answer": "True"},
+    {"text": "Google was originally called 'Backrub'", "answer": "True"}    
+]
