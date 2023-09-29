ef7ef7aafdcf8db97c6c15e81ee3df931c08c395
ahmed adel <71907551+ahmedadel01@users.noreply.github.com>
2023-09-29 15:17:37 +0300

    Create check_if_num.py

---
+++ Day 2/check_if_num.py
@@ -0,0 +1,9 @@
+typed = input("Please type (Yes), (No), or (Maybe) \n")
+if typed == "yes":
+  print(f"You typed {typed}")
+elif typed == "no":
+  print(f"you typed {typed}")
+elif typed == "maybe":
+  print(f"yout typed {typed}")
+else:
+  print("this is not here")
