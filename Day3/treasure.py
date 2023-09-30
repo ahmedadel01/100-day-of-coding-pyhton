cb8a8a2fa8014242e47d55fffd7e74b5bee9bbbc
ahmed adel <71907551+ahmedadel01@users.noreply.github.com>
2023-09-30 21:01:04 +0300

    Update treasure.py

--- Day3/treasure.py
+++ Day3/treasure.py
@@ -2,18 +2,18 @@
 print("Welcome to my island!")
 #ask the user to chose color form red or blue
 print("There are two doors in front of you. a red door and a blue door")
-first-color = input("which door do you want to open? ").lower() 
-if first-color == blue :
+firstcolor = input("which door do you want to open? ").lower() 
+if firstcolor == "blue" :
   print("Ooops! You chose the crocodile door.")
   print("Game over!")
-elif first-color == red :
+elif firstcolor == "red" :
   print("you found three boxes: white, black, green.")
-  second-color = input("Which box do you open? ").lower()
-  if second-color == white:
+  secondcolor = input("Which box do you open? ").lower()
+  if secondcolor == "white":
     print("Ooops! You chose a box filled with snakes.")
-  elif second-color == black:
+  elif secondcolor == "black":
     print("Ooops! You chose a box filled with spiders.")
-  elif second-color == green:
+  elif secondcolor == "green":
     print("Congratulatiions! You found the treasure!")
   else:
     print("Invalid choice")  
